40ab09be1f950ab043d4cfa6bcbd481f66a56626
Carlos Priego <carlos.priego@ibm.com>
2021-01-27 07:52:17 -0600

    chore: scaffold share

---
+++ tests/dummy.test.js
@@ -0,0 +1,9 @@
+// const { dummy } = require('../src/dummy');
+
+describe('Minesweeper Kata: implement the minesweeper game', () => {
+	// beforeEach(() => {});
+
+	it('should pass', () => {
+		expect(true).toBe(true);
+	});
+});
